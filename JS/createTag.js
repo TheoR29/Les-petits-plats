a4a623a0e750a112ee6ea91cea5b8d091344dcf5
Theodore Rieder <totorieder1@gmail.com>
2023-03-17 15:41:34 +0100

    ajout readme

--- JS/createTag.js
+++ JS/createTag.js
@@ -1,34 +1,28 @@
 function addFilterTag(selector, tagClass, closeButtonClass) {
-  fetch('Data/recipes.json')
-  .then(response => response.json())
-  .then(data => {
-    const parsedData = data;
-    const filterTagsContainer = document.getElementById('filter-tags');
+  const filterTagsContainer = document.getElementById('filter-tags');
 
-    // Ajouter un écouteur d'événements à chaque élément de la liste d'ingrédients
-    const ingredientList = document.getElementById(selector);
-    ingredientList.addEventListener('click', (event) => {
-      const clickedIngredient = event.target;
-      if (clickedIngredient.classList.contains('filter__' + tagClass + '--items')) {
-        // Créer un élément de tag
-        const tag = document.createElement('div');
-        tag.classList.add('filter__tag' + tagClass);
-        // Ajouter le nom de l'ingrédient au tag
-        const tagText = document.createElement('span');
-        tagText.classList.add('filter__tag-name');
-        tagText.textContent = clickedIngredient.textContent;
-        tag.appendChild(tagText);
-        // Ajouter le bouton de suppression au tag
-        const closeButton = document.createElement('i');
-        closeButton.classList.add('fa-regular', 'fa-circle-xmark', 'filter__tags-' + closeButtonClass);
-        closeButton.setAttribute('id', 'closeFilterIngredient')
-        tag.appendChild(closeButton);
-        // Ajouter le tag au conteneur de tags
-        filterTagsContainer.appendChild(tag);
-      }
-    });
-
-  })
+  // Ajouter un écouteur d'événements à chaque élément de la liste d'ingrédients
+  const ingredientList = document.getElementById(selector);
+  ingredientList.addEventListener('click', (event) => {
+    const clickedIngredient = event.target;
+    if (clickedIngredient.classList.contains('filter__' + tagClass + '--items')) {
+      // Créer un élément de tag
+      const tag = document.createElement('div');
+      tag.classList.add('filter__tag' + tagClass);
+      // Ajouter le nom de l'ingrédient au tag
+      const tagText = document.createElement('span');
+      tagText.classList.add('filter__tag-name');
+      tagText.textContent = clickedIngredient.textContent;
+      tag.appendChild(tagText);
+      // Ajouter le bouton de suppression au tag
+      const closeButton = document.createElement('i');
+      closeButton.classList.add('fa-regular', 'fa-circle-xmark', 'filter__tags-' + closeButtonClass);
+      closeButton.setAttribute('id', 'closeFilterIngredient')
+      tag.appendChild(closeButton);
+      // Ajouter le tag au conteneur de tags
+      filterTagsContainer.appendChild(tag);
+    }
+  });
 }
 addFilterTag('ingredients', 'ingredients', 'ingredient');
 addFilterTag('Appareils', 'appliances', 'appareil');
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Les-petits-plats
+
+CreateTag : Crée un tag lorsque que l'on clic sur un filtre, par exemple on séléctionne 'Lait de coco', sa crée un tag avec écrie lait de coco avec une croix pour supprimer le tag.
+
+DisplayFilter : Crée la liste des filtres dans chaque composant, c'est à dire Ingredients, Appareils et Ustensiles.
+
+filterIngredient : Ouverture / Fermeture du dropdown, trie des filtres lorsque l'on clic dessus. Ainsi que le trie lorsque l'on clic sur la croix pour supprimer le tag. Il y a aussi la fonction pour rechercher un ingredient / appareil / ustensiles dans chaque dropdown.
+
+index : Création des recettes.
+
+searchBarPrincipal : Contient l'algorithme de recherche pour la barre principal. 
